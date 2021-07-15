a5b17995a64d515df0c7149c725784c61ba4df24
Vinicius André <vinicius.eumesmo.14@gmail.com>
2021-07-14 23:12:00 -0300

    Primeiro Componente

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { Container } from 'react-bootstrap';
+import Teste from './componentes/Teste';
+
+function App() {
+  return (
+    <Teste />
+  );
+}
+
+export default App;
---
+++ src/componentes/Teste.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Teste extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Apex Legends</h1>
+                <img src="https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/apex-featured-image-16x9.jpg.adapt.crop16x9.1023w.jpg" alt="Apex Legends" />
+                <source />
+            </div>
+        );
+    }
+}
+
+export default Teste;
